daad7cafbba0fd0e1694f75a8ea0808a7ed557fa
LcVdk <lc.vanderkelen@gmail.com>
2023-09-28 15:08:04 +0200

    update verifCombo

--- main.js
+++ main.js
@@ -7,25 +7,17 @@ const joueur1 = {
 }
 
 const verifCombo = (de3x) => {
-
-  // for(let elem of combo) {
-     
-  //   let resultVerif = {}
-  //   if(parseInt(de3x) === parseInt(elem["id"])) {
-  //     console.log("elem ID combo :" , elem["id"])
-  //     resultVerif.result  = elem
-  //     resultVerif.message = `vous avez obtenu ${elem["name"]} qui vaut ${elem["nbJeton"]} jetons`
-  //     return resultVerif
-  //      //
-  //   }
-  //   else {
-  //     resultVerif.message = `vous avez obtenu une combinaison basique qui vaut 1 jeton`
-  //     return resultVerif
-  //   }
-  // }
   let compaCombo =  combo.filter((x) => x.id === de3x  )
   console.log("compacombo : " , compaCombo)
-
+  if(compaCombo.length === 0)  {
+    let infoDe = {};
+    infoDe.id = de3x;
+    infoDe.name = `une combinaison basique`;
+    infoDe.nbJeton = 1;
+    compaCombo.push(infoDe)
+    return compaCombo 
+  }
+    else return compaCombo
 }
   
 
@@ -45,24 +37,29 @@ const jouerDe = (min, max) => {
 const charge = (joueur) => {
   
   let combinaison = []
+  //jouer les 3 dés
   joueur["de1"] = jouerDe(1,7)
   joueur["de2"] = jouerDe(1,7)
   joueur["de3"] = jouerDe(1,7)
+  //check des résultats
   console.log("après : ", joueur["de1"])
   console.log("après : ", joueur["de2"])
   console.log("après : ", joueur["de3"])
+  //ajoute les valeurs de chaque dé dans le tableau combinaison
   combinaison.push(joueur["de1"]);
   combinaison.push(joueur["de2"]);
   combinaison.push(joueur["de3"]);
-
   console.log("raw :", combinaison)
-  let sortedJoined = parseInt(combinaison.sort().reverse().join(''))
-  joueur.sortedCombo = sortedJoined
   
+  //copie du tableau trié de manière décroissante, et fusionné en un nombre
+  let sortedJoined = parseInt(combinaison.sort().reverse().join(''))
+  
+  //ajout du nombre a l'attibut de l'objet joueur
+  joueur.sortedCombo = sortedJoined
   console.log("sorted :", sortedJoined)
 
-
-  joueur.resultatTxt = verifCombo(parseInt(sortedJoined))
+  // 
+  joueur.resultatTxt = verifCombo(sortedJoined)
 
   console.log(joueur)
 
